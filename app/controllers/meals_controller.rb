9c324afa246fea8e112e888e5518d1e4843bcc24
antimonysayz <antimonysayz@gmail.com>
2020-10-13 23:48:30 -0400

    testing post routes for meals

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,19 @@
 class MealsController < ApplicationController
+
+    def create
+        meal = Meal.create(name: params[:name])
+        render json: meal
+    end
+
+    def update
+        meal = Meal.find_by(id: params[:id])
+        meal.update(name: params[:name], ingredients: params[:ingredients])
+        render json: meal
+    end
+
+    def index
+        meals = Meal.all
+        render json: meals
+    end
+
 end
